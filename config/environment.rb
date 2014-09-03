ae5a5fe7d14244276dec90df502f9e4b61f5196a
Jamil Velji <jamil@jamilvelji.com>
2014-09-02 21:55:30 -0400

    Heroku fix

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-# For Devise and Heroku
-config.action_mailer.default_url_options = { :host => 'orm-pinteresting.herokuapp.com' }
